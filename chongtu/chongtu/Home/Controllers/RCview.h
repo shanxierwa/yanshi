f7dcff5b40332b324c7f4000afe6753f2aaffcef
rujianbin <328989397@qq.com>
2016-10-23 21:56:56 +0800

    11

---
+++ chongtu/chongtu/Home/Controllers/RCview.h
@@ -0,0 +1,13 @@
+//
+//  RCview.h
+//  chongtu
+//
+//  Created by 茹 on 16/10/23.
+//  Copyright © 2016年 ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCview : UIView
+
+@end
---
+++ chongtu/chongtu/Home/Controllers/RCview.m
@@ -0,0 +1,21 @@
+//
+//  RCview.m
+//  chongtu
+//
+//  Created by 茹 on 16/10/23.
+//  Copyright © 2016年 ru. All rights reserved.
+//
+
+#import "RCview.h"
+
+@implementation RCview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
