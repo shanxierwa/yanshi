8b0aa9ff5a4b52eeed0c0921f56f14d9e3c1ce88
ru <ru@rudeMacBook-Pro.local>
2016-10-23 21:09:11 +0800

    2

--- chongtu/chongtu/RBViewController.m
+++ chongtu/chongtu/RBViewController.m
@@ -1,25 +1,23 @@
 //
-//  ViewController.m
+//  RBViewController.m
 //  chongtu
 //
 //  Created by ru on 16/10/23.
 //  Copyright © 2016年 ru. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "RBViewController.h"
 
-@interface ViewController ()
+@interface RBViewController ()
 
 @end
 
-@implementation ViewController
+@implementation RBViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    self.title = @"one";
-    self.view.backgroundColor = [UIColor redColor];
-    
+    self.title = @"Two";
+    self.view.backgroundColor = [UIColor yellowColor];
     
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeContactAdd];
     btn.center = self.view.center;
@@ -30,8 +28,5 @@
 - (void)btnClick:(UIButton *)btn {
     NSLog(@"11");
 }
-
-
-
 
 @end
