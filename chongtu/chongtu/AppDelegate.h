fedb4755c12915ca5a20918367c55ffa25bffc18
ru <ru@rudeMacBook-Pro.local>
2016-10-23 20:34:37 +0800

    创建项目

---
+++ chongtu/chongtu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chongtu
+//
+//  Created by ru on 16/10/23.
+//  Copyright © 2016年 ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chongtu/chongtu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chongtu
+//
+//  Created by ru on 16/10/23.
+//  Copyright © 2016年 ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
