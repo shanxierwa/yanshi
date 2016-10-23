f08c243f575f1550b5371396e9aa99788a3f70f5
rujianbin <328989397@qq.com>
2016-10-23 22:13:56 +0800

    回滚

--- chongtu/chongtu/ViewController.m
+++ chongtu/chongtu/ViewController.m
@@ -16,9 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"主线上有这句话");
-    NSLog(@"主线又来句");
-
 
     self.title = @"one";
     self.view.backgroundColor = [UIColor redColor];
@@ -28,12 +25,6 @@
     btn.center = self.view.center;
     [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
-    
-    
-    //还是和主线
-}
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    NSLog(@"点击");
 }
 
 - (void)btnClick:(UIButton *)btn {
