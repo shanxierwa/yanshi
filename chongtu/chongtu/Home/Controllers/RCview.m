161c82da462633448ee9585d7d0608f2978f5deb
rujianbin <328989397@qq.com>
2016-10-23 21:58:59 +0800

    22

--- chongtu/chongtu/Home/Controllers/RCview.m
+++ chongtu/chongtu/Home/Controllers/RCview.m
@@ -10,12 +10,13 @@
 
 @implementation RCview
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"111");
+    }
+    return self;
 }
-*/
 
 @end
